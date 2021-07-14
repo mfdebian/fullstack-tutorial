bbda841f195050244b6e438f2a950c9464894717
Alfredo González <alfredo.arosteguy@gmail.com>
2021-07-14 12:51:36 -0400

    Set up Apollo client, fetch data with queries, update data with mutations and manage local state of the React App

--- start/client/src/cache.ts
+++ start/client/src/cache.ts
@@ -1,3 +1,51 @@
-import { InMemoryCache, Reference } from '@apollo/client';
+import { InMemoryCache, Reference, makeVar } from '@apollo/client';
 
-export const cache: InMemoryCache = new InMemoryCache({});
+export const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        /*
+        Our two field policies each include a single field: a read
+        function. Apollo Client calls a field's read function
+        whenever that field is queried. The query result uses
+        the function's return value as the field's value,
+        regardless of any value in the cache or on your
+        GraphQL server.
+        */
+        isLoggedIn: {
+          read() {
+            return isLoggedInVar();
+          }
+        },
+        cartItems: {
+          read() {
+            return cartItemsVar();
+          }
+        },
+        launches: {
+          keyArgs: false,
+          merge(existing, incoming) {
+            let launches: Reference[] = [];
+            if (existing && existing.launches) {
+              launches = launches.concat(existing.launches);
+            }
+            if (incoming && incoming.launches) {
+              launches = launches.concat(incoming.launches);
+            }
+            return {
+              ...incoming,
+              launches,
+            };
+          }
+        }
+      }
+    }
+  }
+});
+
+// Initializes to true if localStorage includes a 'token' key,
+// false otherwise
+export const isLoggedInVar = makeVar<boolean>(!!localStorage.getItem('token'));
+
+// Initializes to an empty array
+export const cartItemsVar = makeVar<string[]>([]);
